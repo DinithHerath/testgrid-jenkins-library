58aed57c185399c11cb319f06f8676f43d4a6f78
Dinith Herath <41161459+DinithHerath@users.noreply.github.com>
2023-05-19 10:41:16 +0530

    Change pipeling agent

--- main.groovy
+++ main.groovy
@@ -26,7 +26,7 @@ def s3BuildLogPath = ""
 def s3PathConstructor = ""
 
 pipeline {
-agent {label 'pipeline-agent'}
+agent {label 'pipeline-agent-2'}
 stages {
     stage('Clone CFN repo') {
         steps {
